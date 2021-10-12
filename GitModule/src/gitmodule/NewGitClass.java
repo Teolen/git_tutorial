8b72e6d4c6dfbfd1c974306cdd4dbdcc173e639d
Serhii Yakovliuk <serhii.y.m@gmail.com>
2021-10-12 19:53:07 +0300

    Added some work

---
+++ GitModule/src/gitmodule/NewGitClass.java
@@ -0,0 +1,20 @@
+package gitmodule;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class NewGitClass {
+    public static void main(String[] args) throws IOException {
+        ExecutorService exec = Executors.newSingleThreadExecutor();
+        exec.execute(new Runnable() {
+            @Override
+            public void run() {
+                while(true)
+                    System.out.println("i'm alive");
+            }
+        });
+        System.in.read();
+        System.exit(0);
+    }
+}
